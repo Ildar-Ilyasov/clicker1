118d06a627b7fe40a83cb8847781faeb67e85bed
Ildar-Ilyasov <ildar.ilyasov123@yandex.com>
2023-02-25 00:23:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/clicker1/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.clicker1;
+
+
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.clicker1.R;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "StartActivity";
+    private Integer counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", counter);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey("counter")) {
+            counter = savedInstanceState.getInt("counter");
+        }
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+
+    private void resetUI() {
+        ((TextView) findViewById(R.id.txt_counter)).setText(counter.toString());
+        Log.d(TAG, "resetUI");
+    }
+
+    public void onClickBtnAddRub(View view) {
+        counter++;
+        TextView counterView = findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+    }
+}
